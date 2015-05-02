46abebba74b49a8b81f7564c9a9a2e84cd677cdb
blatendr <blatendr@uvm.edu>
2015-05-02 17:51:30 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php 
+include "header.php";
+include "nav.php";
+?>
+
+<h1>Welcome to our website!</h1>
+<p>Hip Hop News you can Use is a a handy web site that keep you up to date on current hip hop artists and albums.
+You can find info on past albums of 2014, albums that have been released this year and upcoming albums that are yet to drop. 
+Also you will find other music suggestions in the About Us page that are most totally worth a look up.
+Hip Hop News you can Use is a a handy web site that keep you up to date on current hip hop artists and albums.
+You can find info on past albums of 2014, albums that have been released this year and upcoming albums that are yet to drop. 
+Also you will find other music suggestions in the About Us page that are most totally worth a look up.
+</p>
