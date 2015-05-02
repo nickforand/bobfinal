214674f68ceb060788facab45bc084fa0c58b1d7
nickforand <nforand@uvm.edu>
2015-05-02 17:53:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include "header.php";
 include "nav.php";
 ?>
-
+<body>
 <h1>Welcome to our website!</h1>
 <p>Hip Hop News you can Use is a a handy web site that keep you up to date on current hip hop artists and albums.
 You can find info on past albums of 2014, albums that have been released this year and upcoming albums that are yet to drop. 
@@ -11,3 +11,4 @@ Hip Hop News you can Use is a a handy web site that keep you up to date on curre
 You can find info on past albums of 2014, albums that have been released this year and upcoming albums that are yet to drop. 
 Also you will find other music suggestions in the About Us page that are most totally worth a look up.
 </p>
+</body>
