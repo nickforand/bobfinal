9c5bf23815542b776cd930a67921e92a7d620732
blatendr <blatendr@uvm.edu>
2015-05-03 19:47:39 -0400

    Create future.php

---
+++ future.php
@@ -0,0 +1,49 @@
+<?php 
+include "nav.php";
+?>
+
+<h1>Up and Coming Albums to Look out For!</h1>
+<p>This has been a good year for releases but there are many for exciting albums to come!</p>
+
+<content>
+    <h2>Frank Ocean's <i>Boys Don't Cry</i>(?)</h2>
+    <p>
+        Frank Ocean is a singer and song writer from California that has made a big name for himself after his debut album <i>Nostalgia, Ultra</i>
+        and his second release <i>Channel Orange.</i> Frank takes on a modern R&B feel to his music and has been hinting at releasing the possibly named<i>Boys Don't Cry</i>
+        for awhile now. When this album drops definitely check it out. Here is a song of his last album titled "Thinkin Bout You."
+        
+    </p>    
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/iHG0hj64JS8" frameborder="0" allowfullscreen>
+            
+        </iframe>
+</content>
+
+<content>
+    <h2>Kanye West's <i>So Help Me God</i></h2>
+    <p>
+        The music mogul Kayne West is back at it again and is on the verge of releasing his 8th commercial album and is collaborating with 
+        artists such as Paul McCartney and Vic Mensa. Anything Kayne puts out is interesting due to his inflated ego and music ability. A couple
+        singles have been released from the album so it is likely to drop soon. Here is one single off <i>So Help Me God,</i> "All Day."    
+    </p>
+    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/UfQOawFwx6w" frameborder="0" allowfullscreen>
+        
+    </iframe>
+    
+</content>
+
+<content>
+    <h2>Chance the Rapper and the Social Experiment's <i>Surf</i></h2>
+    <p>
+        Chance the Rapper stormed the scene with his last album <i>Acid Rap</i> which was released in 2013. Chance has more recently 
+        joined the group "The Social Experiment" and they have leaked a couple singles off of it including the song "Sunday Candy." 
+        I will be interesting to see what they put out and definitely keep your ear out for chance. 
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/i4ooH8frBWg" frameborder="0" allowfullscreen>
+      
+    </iframe>
+    
+    
+    
+    
+</content>
