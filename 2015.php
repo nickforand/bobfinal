790c1d4a0abe455e5655854189e898fc27b62ab1
blatendr <blatendr@uvm.edu>
2015-05-03 19:47:02 -0400

    Create 2015.php

---
+++ 2015.php
@@ -0,0 +1,54 @@
+<?php 
+include "header.php";
+
+include "nav.php";
+?>
+
+<h1>Albums that came out this year</h1>
+
+
+
+<content>
+<h2>Joey Bada$$'s <i>B4.Da.$$</i></h2>
+
+<p>This album dropped January 20 with an astounding force and proved that Joey Badass
+    is still relevant in the rap game. Songs like "Paper Trail$" and "Christ Conscious"
+have the vigor of younger Joey's style. I rate this album a 7.6/10, all around it is solid 
+conceptually and musically</p>
+
+<iframe width="600" height="450" 
+        src="https://www.youtube.com/embed/NFcJI3EeEbs" frameborder="0" allowfullscreen>
+            
+</iframe>
+ 
+</content>
+
+<content>
+    <h2>BADBADNOTGOOS and Ghostface Killah's <i>Sour Soul</i></h2>
+    <p>
+        <i>Sour Soul</i> was a ground breaking collaborative between classic East Coast rapper Ghostface Killah
+        from the Wu Tang Clan and BBNG (BADBADNOTGOOD) who are a jazz trio that are taking the Hip Hop scene by storm. The jazzy feel
+        brought out by BBNG mixes extremely uniquely with Ghostface's in your face style and creates a masterpiece of an album. I would 
+        rate <i>Sour Soul</i> a 6.8/10 because the two artists chemistry is still a little shaky but expected.
+    </p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/cod_6dsIsnE" frameborder="0" allowfullscreen>
+            
+        </iframe>
+        
+    
+    
+    
+</content>
+
+<content>
+    <h2>Drakes's <i>If You're Reading This it's to Late</i></h2>
+    <p>
+        Pop star Drake released his sixth album on Feb 13. The album features Lil Wayne, PARTYNEXTDOOR and
+        Travi$ Scott and it is one of the biggest commercial album of the year selling 835,000 copies. The songs "Energy"
+        and "Legend" are some of the best songs Drake have produced and this album is definitely worth a once listen through.
+        
+    </p>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/cGA9iVcpaJ8" frameborder="0" allowfullscreen>
+        
+    </iframe>
+</content>
