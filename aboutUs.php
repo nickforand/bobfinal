0d9e812c60a646a7a47953d863137578c75b5567
nickforand <nforand@uvm.edu>
2015-05-03 19:40:58 -0400

    Update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -16,7 +16,7 @@
             <h1>A Look at the Gurus of Hip-Hop News You Can Use!</h1>
             <h2>Nick Forand</h2>
             <p>Hello, my name is Nick Forand and I am one of the editors on “Hip-Hop News You Can Use”. I am student at the University of Vermont and have been an avid listener of rap music for many years. My all time favorite rap album is <i>2001</i> by Dr. Dre. My favorite release this year has got to be <i> 2014 Forest Hills Drive</i>.    </p>
-        
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/sFvoIeSzChs" frameborder="0" allowfullscreen></iframe>
         
         
         
