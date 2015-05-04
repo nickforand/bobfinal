2d61d30442e23d822d67ca528670977fc626bff1
blatendr <blatendr@uvm.edu>
2015-05-03 21:56:53 -0400

    Update header.php

--- header.php
+++ header.php
@@ -1,3 +1,5 @@
 <header>
-<h1>Hip-Hop News You Can Use!</h1>
+    <h1>Hip-Hop News You Can Use!</h1> <img src="record.png" alt="icon for the site"/>
+                                            
+    
 </header>
