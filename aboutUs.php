a594bf79b8cdb1aa656ce166d7a61428a95bc25f
blatendr <blatendr@uvm.edu>
2015-05-03 21:10:42 -0400

    Update aboutUs.php

--- aboutUs.php
+++ aboutUs.php
@@ -1,24 +1,18 @@
-<?php
-        include("top.php");
+<?php 
+ include "header.php";
+include "nav.php";
+include "top.php";
 ?>
-        
-<?php
-            include("header.php");
-        ?>
-    <nav>
-         <?php
-            include("nav.php");
-        ?>   
-            
-            
-        </nav>
+
+
+           
         <body>
-            <h1>A Look at the Gurus of Hip-Hop News You Can Use!</h1>
-            <h2>Nick Forand</h2>
+            <h2>A Look at the Gurus of Hip-Hop News You Can Use!</h2>
+            
             <p>Hello, my name is Nick Forand and I am one of the editors on “Hip-Hop News You Can Use”. I am student at the University of Vermont and have been an avid listener of rap music for many years. My all time favorite rap album is <i>2001</i> by Dr. Dre. My favorite release this year has got to be <i> 2014 Forest Hills Drive</i>.    </p>
             <iframe width="560" height="315" src="https://www.youtube.com/embed/sFvoIeSzChs" frameborder="0" allowfullscreen></iframe>
         
-        
+            <p>Hi, my name is Brad Latendresse and I am one of the creators and users of "Hip-Hop News You can Use." Check out this site for hot new music and for album drops to look forward too. One of the albums I am most excited for this year is <i>Boys Don't Cry</i> by Frank Ocean</p>
         
         
         </body>
